56d4eddb4dc080d0054cd53bda2addb81800c85c
AndreiMariusZed <62403550+AndreiMariusZed@users.noreply.github.com>
2021-04-22 16:16:15 +0300

    Index.html

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require("dotenv");
+const path = require('path');
 require('dotenv/config');
 
 //Init app
 const app = express();
-
+app.use(express.static(path.join(__dirname, 'public')))
 //Body Parser Middleware
 app.use(bodyParser.urlencoded({extended: false}))
 
@@ -26,15 +27,18 @@ mongoose.connect(
 const usersRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 
+//Load View Engine
+// app.set('views',path.join(__dirname,'views'));
+// app.set('view engine','pug');
+
 //Route Middlewares
 app.use('/api/users', usersRoute);
 app.use('/api/user', authRoute);
 
 //Home route
-app.get('/', (req, res) => {
-    res.send('Test!!!');
-});
-
+app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname+'/index.html'));
+  });
 //Start server
 app.listen(3000, () => 
     console.log('Server up and running!')
