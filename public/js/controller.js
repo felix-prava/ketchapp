d38f1f06c96c9134ef62c1e2bb3ef570b113abfc
AndreiMariusZed <62403550+AndreiMariusZed@users.noreply.github.com>
2021-05-03 23:39:07 +0300

    Add helpers.js

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,41 +1,24 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
-
-
 const showRecipe = async function (){
   try {
     let recipeID = window.location.hash;
     recipeID = recipeID.slice(1);
-
     //Check to have a valid id
     if (!recipeID) 
       return;
       recipeView.renderSpinner();
-
     //Loading recipe
     await model.loadRecipe(recipeID);
-    
     //Rendering recipe
     recipeView.render(model.state.recipe);
-
-    
   } catch (err) {
     alert(err);
   }
 };
 showRecipe();
-
 //Event listener for showing a recipe after selecting it
 window.addEventListener('hashchange', showRecipe);
-
 //Event listener for showing a recipe after loading a page with one
 window.addEventListener('load', showRecipe);
--- public/js/model.js
+++ public/js/model.js
@@ -1,18 +1,13 @@
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js'
+
 export const state = {
     recipe: {},
 };
 
 export const loadRecipe = async function(recipeID) {
     try{
-        const res = await fetch(
-            //'http://localhost:3000/api/recipes/60899b7c63879f112092fa09'
-            //'http://localhost:3000/api/recipes/608e735c7b4f6930d8a5e5fa'
-            `http://localhost:3000/api/recipes/${recipeID}`
-        );
-        const data = await res.json();
-    
-        if (!res.ok) throw new Error(`${data.message} (${res.status})`)
-        console.log(res, data);
+        const data = await getJSON(`${API_URL}/${recipeID}`);
         let recipe = data;
         state.recipe = {
             id: recipe._id,
