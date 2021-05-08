a01ebe5218fabbd71effccb1f2ee5c01983e2b35
AndreiMariusZed <62403550+AndreiMariusZed@users.noreply.github.com>
2021-05-08 19:23:54 +0300

    dynamic pagination

--- public/js/controller.js
+++ public/js/controller.js
@@ -35,8 +35,7 @@ const controlSearchResults = async function() {
     
     // Load the model state with the types of food
     await model.loadSearchResults(query);
-    // resultsView.render(model.state.search.results);
-    resultsView.render(model.getSearchResultsPage(1));
+    resultsView.render(model.getSearchResultsPage());
   } catch (err) {
     console.log(err);
   }
