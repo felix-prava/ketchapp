01f30f8ff57ee3dd34db762b1c3d0b77bcf6f459
AndreiMariusZed <62403550+AndreiMariusZed@users.noreply.github.com>
2021-05-10 19:40:34 +0300

    add favourites to local storage

--- public/js/views/favouritesView.js
+++ public/js/views/favouritesView.js
@@ -5,6 +5,10 @@ class FavouritesView extends View{
     _parentElement = document.querySelector('.favourites__list');
     _errorMessage = `No favourites yet. Find a nice recipe and add it to your list!`; 
     _successMessage = `Congrats! It's all good`;
+
+    addHandlerRender (handler) {
+        window.addEventListener('load', handler);
+    }
     
     _createMarkup() {
         return this._data.map(favourite => previewView.render(favourite, false)).join('');
