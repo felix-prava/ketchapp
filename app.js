7f40386dc9adb51a6916bdb2d1d802240b04231e
felix24mihai <mihai_prava@yahoo.com>
2021-05-11 21:17:49 +0300

    Add login/register routes

--- app.js
+++ app.js
@@ -6,25 +6,25 @@ const dotenv = require("dotenv");
 const path = require('path');
 require('dotenv/config');
 
-//Init app
+// Init app
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')))
 
-//Body Parser Middleware
+// Body Parser Middleware
 app.use(bodyParser.urlencoded({extended: false}))
 
-//Parse application/json
+// Parse application/json
 app.use(bodyParser.json());
 
 app.use(cors());
 
-//Connect to DB
+// Connect to DB
 mongoose.connect(
     process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => 
     console.log('Connected to DB!')
 );
 
-//Import routes
+// Import routes
 const usersRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const recipesRoute = require('./routes/recipes');
@@ -33,17 +33,34 @@ const recipesRoute = require('./routes/recipes');
 // app.set('views',path.join(__dirname,'views'));
 // app.set('view engine','pug');
 
-//Route Middlewares
+// Route Middlewares
 app.use('/api/users', usersRoute);
 app.use('/api/user', authRoute);
 app.use('/api/recipes', recipesRoute);
 
-//Home route
+// Home route
 app.get('/',function(req,res) {
     res.sendFile(path.join(__dirname+'/index.html'));
-  });
+});
 
-//Start server
+// Home route for a logged in user
+app.get('/home',function(req,res) {
+    res.sendFile(path.join(__dirname+'/public/home.html'));
+});
+
+// Route for the login page
+app.get('/login',function(req,res) {
+    res.sendFile(path.join(__dirname+'/public/login.html'));
+});
+
+// Route for the register page
+app.get('/register',function(req,res) {
+    res.sendFile(path.join(__dirname+'/public/register.html'));
+});
+
+
+
+// Start server
 app.listen(3000, () => 
     console.log('Server up and running!')
 );
